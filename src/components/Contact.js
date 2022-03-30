f755d89fa24e143fd7359617741412152e11fa93
paulsalamone <psalamone@gmail.com>
2022-03-30 22:09:06 +0200

    basic form styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
+const FORM_ENDPOINT = "";
+
 const Contact = () => {
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <>
       <section className="flex flex-col items-center mt-8">
@@ -9,6 +19,49 @@ const Contact = () => {
           </h2>
           <div className="border-b-2 border-dotted w-2/5"></div>
         </div>
+        {/* CONTACT FORM */}
+        <form
+          onSubmit={handleSubmit}
+          method="POST"
+          target="_blank"
+          className="flex flex-col items-center w-full mb-10"
+        >
+          <input
+            type="text"
+            name="name"
+            placeholder="your name"
+            required
+            className="border-2 border-yellow-300 bg-stone-800 p-4 m-3 text-white font-heading w-96"
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="email address"
+            required
+            className="border-2 border-yellow-300 bg-stone-800 p-4 m-3 text-white font-heading w-96"
+          />
+          <textarea
+            cols="30"
+            rows="5"
+            name="message"
+            placeholder="write me a message!"
+            required
+            className="border-2 border-yellow-300 bg-stone-800 p-4 m-3 text-white font-heading w-96 "
+          ></textarea>
+          <button
+            type="submit"
+            className="border-2 border-yellow-600 bg-yellow-300 p-3 rounded-md  m-3 text-stone-800 font-bold font-heading w-48 text-lg hover:bg-yellow-200 bg:border-yellow-500
+            hover:translate-x-[-1px] 
+            hover:translate-y-[-1px] 
+            active:translate-x-[1px] 
+            active:translate-y-[1px] 
+            transition-all
+            cursor-pointer"
+          >
+            {" "}
+            submit
+          </button>
+        </form>
       </section>
     </>
   );
